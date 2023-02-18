4a9ad9be8904f3dc3bfb080e6e6fb627cedb70a4
JohnsonLee <0xjohnsonlee@gmail.com>
2023-02-18 22:23:27 +0800

    turn init.vim into lua

--- init.lua
+++ init.lua
@@ -1,22 +1,19 @@
---  local conf_root = '~/.conf/nvim'
---  local vimplug = conf_root .. 'autoload/plug.vim'
-
-vim.cmd([[
-  let conf_root='~/.config/nvim/'
-  let vimplug=conf_root . 'autoload/plug.vim'
-  if empty(glob(vimplug))
-            silent call system('curl -fLo ' . vimplug . ' --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim')
-            autocmd VimEnter * PlugInstall --sync | source $MYVIMRC
-  endif
-
-  set runtimepath^=~/.config/nvim runtimepath+=~/.config/nvim/after
-  let &packpath=&runtimepath
-  source ~/.config/nvim/general.vim
-  source ~/.config/nvim/mycommand.vim
-  source ~/.config/nvim/plug_list.vim
-  source ~/.config/nvim/plugconf.vim
-  source ~/.config/nvim/apperance.vim
-  source ~/.config/nvim/keybindings.vim
-  luafile ~/.config/nvim/treesitter.lua
-  let conceallevel=3
-]])
+conf_root = vim.fn.expand('~/.config/nvim/')
+vimplug = conf_root .. 'autoload/plug.vim'
+if vim.fn.empty(vim.fn.glob(vimplug)) ~= 0 then
+  os.execute('curl -fLo ' .. vimplug .. ' --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim')
+  vim.api.nvim_create_autocmd({"VimEnter"}, {
+    pattern = {"*"},
+    command = "PlugInstall -- sync | source $MYVIMRC"
+  })
+end
+vim.o.runtimepath = table.concat( { conf_root, vim.o.runtimepath, conf_root..'after' } , ",")
+vim.o.packpath = vim.o.runtimepath
+vim.o.conceallevel = 3
+vim.cmd.source('~/.config/nvim/general.vim')
+vim.cmd.source('~/.config/nvim/mycommand.vim')
+vim.cmd.source('~/.config/nvim/plug_list.vim')
+vim.cmd.source('~/.config/nvim/plugconf.vim')
+vim.cmd.source('~/.config/nvim/apperance.vim')
+vim.cmd.source('~/.config/nvim/keybindings.vim')
+vim.cmd.source('~/.config/nvim/treesitter.lua')
