10bbf29680c91376794adee886744043127133e6
Li Junxian <0xjohnsonlee@gmail.com>
2023-08-06 18:41:54 +0800

    replace keybindings of move.nvim to be compatible with zellij

--- lua/keybindings/move.lua
+++ lua/keybindings/move.lua
@@ -1,14 +1,14 @@
 local opts = { noremap = true, silent = true }
 -- Normal-mode commands
-vim.keymap.set('n', '<A-j>', ':MoveLine(1)<CR>', opts)
-vim.keymap.set('n', '<A-k>', ':MoveLine(-1)<CR>', opts)
-vim.keymap.set('n', '<A-h>', ':MoveHChar(-1)<CR>', opts)
-vim.keymap.set('n', '<A-l>', ':MoveHChar(1)<CR>', opts)
+vim.keymap.set('n', '<C-j>', ':MoveLine(1)<CR>', opts)
+vim.keymap.set('n', '<C-k>', ':MoveLine(-1)<CR>', opts)
+vim.keymap.set('n', '<C-h>', ':MoveHChar(-1)<CR>', opts)
+vim.keymap.set('n', '<C-l>', ':MoveHChar(1)<CR>', opts)
 vim.keymap.set('n', '<leader>wf', ':MoveWord(1)<CR>', opts)
 vim.keymap.set('n', '<leader>wb', ':MoveWord(-1)<CR>', opts)
 
 -- Visual-mode commands
-vim.keymap.set('v', '<A-j>', ':MoveBlock(1)<CR>', opts)
-vim.keymap.set('v', '<A-k>', ':MoveBlock(-1)<CR>', opts)
-vim.keymap.set('v', '<A-h>', ':MoveHBlock(-1)<CR>', opts)
-vim.keymap.set('v', '<A-l>', ':MoveHBlock(1)<CR>', opts)
+vim.keymap.set('v', '<C-j>', ':MoveBlock(1)<CR>', opts)
+vim.keymap.set('v', '<C-k>', ':MoveBlock(-1)<CR>', opts)
+vim.keymap.set('v', '<C-h>', ':MoveHBlock(-1)<CR>', opts)
+vim.keymap.set('v', '<C-l>', ':MoveHBlock(1)<CR>', opts)
