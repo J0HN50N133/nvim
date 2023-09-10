bdcbbdba10b2a21984355934898d6f1f25fc8cb8
JohnsonLee <0xjohnsonlee@gmail.com>
2023-09-10 14:43:12 +0800

    bootstrap: add codelldb

--- lua/bootstrap.lua
+++ lua/bootstrap.lua
@@ -1,7 +1,8 @@
 local bin_path = vim.fn.stdpath('data') .. '/bin'
-local ghproxy = 'https://ghproxy.com/'
+local ghproxy = (function(need_proxy) if need_proxy then return 'https://ghproxy.com/' else return '' end end)(true)
 local fmt = string.format
 vim.fn.setenv("PATH", bin_path .. ":" .. vim.fn.getenv("PATH"))
+
 local function boot_lazy()
         local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
         if not vim.loop.fs_stat(lazypath) then
@@ -21,9 +22,9 @@ local function boot_rg()
         local rg_path = bin_path .. '/rg'
         if not vim.loop.fs_stat(rg_path) then
                 local VERSION = '13.0.0'
-                local URL = fmt(
-                        '%sgithub.com/BurntSushi/ripgrep/releases/download/%s/ripgrep-%s-x86_64-unknown-linux-musl.tar.gz',
-                        ghproxy, VERSION, VERSION)
+                local URL = ghproxy .. fmt(
+                        'github.com/BurntSushi/ripgrep/releases/download/%s/ripgrep-%s-x86_64-unknown-linux-musl.tar.gz',
+                        VERSION, VERSION)
                 os.execute('mkdir -p ' .. bin_path)
                 os.execute(fmt("curl -sL %s | tar -xzf - -C %s --wildcards '*/rg' --strip=1", URL, bin_path))
         end
@@ -31,7 +32,8 @@ end
 
 local function boot_node()
         local node_path = bin_path .. '/nodejs'
-        if not vim.loop.fs_stat(node_path .. '/bin/node') then
+        local exe_path = node_path .. '/bin/node'
+        if not vim.loop.fs_stat(exe_path) then
                 local VERSION = 'v18.17.1'
                 local DISTRO = 'linux-x64'
                 local URL = fmt('https://mirrors.tuna.tsinghua.edu.cn/nodejs-release/%s/node-%s-%s.tar.xz',
@@ -42,6 +44,25 @@ local function boot_node()
         vim.fn.setenv("PATH", node_path .. '/bin' .. ":" .. vim.fn.getenv("PATH"))
 end
 
+local function boot_lldb()
+        local codelldb_path = bin_path .. '/codelldb'
+        local exe_path = codelldb_path .. '/extension/adapter/codelldb'
+        if not vim.loop.fs_stat(exe_path) then
+                local VERSION = 'v1.9.2'
+                local ARCH = 'x86_64'
+                local OS = 'linux'
+                local URL = ghproxy ..
+                    fmt('github.com/vadimcn/codelldb/releases/download/%s/codelldb-%s-%s.vsix', VERSION,
+                            ARCH, OS)
+                -- install
+                local tmpname = os.tmpname()
+                os.execute(fmt('wget -qO %s %s', tmpname, URL))
+                os.execute(fmt('unzip %s -d %s', tmpname, codelldb_path))
+        end
+        vim.fn.setenv("PATH", codelldb_path .. '/extension/adapter' .. ":" .. vim.fn.getenv("PATH"))
+end
+
 boot_lazy()
 boot_rg()
 boot_node()
+boot_lldb()
