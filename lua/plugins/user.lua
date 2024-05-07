12b2bdb4abd55308db1e2e1a6214869aecec061c
JohnsonLee <0xjohnsonlee@gmail.com>
2024-05-07 22:14:59 +0800

    disable lsp_signature.nvim

--- lua/plugins/user.lua
+++ lua/plugins/user.lua
@@ -6,13 +6,6 @@ return {
 
   -- == Examples of Adding Plugins ==
 
-  -- "andweeb/presence.nvim",
-  {
-    "ray-x/lsp_signature.nvim",
-    event = "BufRead",
-    config = function() require("lsp_signature").setup() end,
-  },
-
   {
     "kylechui/nvim-surround",
     event = "VeryLazy",
